719a7a6aa20ecc61bad5cabd33c89d2618fd19fd
David Nguyen <nguyendavid332@gmail.com>
2023-05-03 13:37:13 +0100

    Added logic for game won and lost

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,6 @@
 from tabulate import tabulate
 import numpy as np
+import math
 
 class GameLogic:
 
@@ -9,6 +10,7 @@ class GameLogic:
         self._size = size
         self._board = np.zeros((self._size, self._size))
         self._initialize_board()
+        # self._board = np.array([[2, 4, 8, 16], [4, 8, 16, 32], [8, 16, 32, 64], [16, 32, 64, 128]])
 
     def _initialize_board(self):
         starting_tiles = np.random.choice(GameLogic.TILE_DISTRIBUTION, 2)
@@ -68,6 +70,15 @@ class GameLogic:
         self._board = np.rot90(self._board, -1)
         self.move_left()
         self._board = np.rot90(self._board, 1)
+
+    def _game_lost(self):
+        diff_rows = np.diff(self._board, axis=0)
+        diff_columns = np.diff(self._board, axis=1)
+        return False if np.any(diff_columns == 0) or np.any(diff_rows == 0) else True
+
+    def _game_won(self):
+        winning_tile = math.pow(2, self._size + 7)
+        return True if np.any(self._board == winning_tile) else False
 
 gl = GameLogic()
 gl.print_board()
