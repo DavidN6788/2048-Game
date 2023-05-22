67c1fcc790f56964a1a52a3520a6fc2514dca9d6
David Nguyen <nguyendavid332@gmail.com>
2023-05-22 12:31:30 +0100

    Added test file

---
+++ test_game_logic.py
@@ -0,0 +1,87 @@
+import pytest
+import game_logic
+
+class TestMoveLeft:
+
+    def test_move_left(self):
+        assert False
+
+    def test_move_left_on_fixed_board(self):
+        assert False
+
+    def test_move_left_and_game_lost(self):
+        assert False
+
+    def test_move_left_and_game_won(self):
+        assert False
+
+    def test_move_left_prev_board_for_non_fixed_board(self):
+        assert False
+
+    def test_move_left_prev_board_for_fixed_board(self):
+        assert False
+
+class TestMoveRight:
+
+    def test_move_right(self):
+        assert False
+
+    def test_move_right_on_fixed_board(self):
+        assert False
+
+    def test_move_right_and_game_lost(self):
+        assert False
+
+    def test_move_right_and_game_won(self):
+        assert False
+
+    def test_move_right_prev_board_equals_non_fixed_board(self):
+        assert False
+
+    def test_move_right_prev_board_equals_fixed_board(self):
+        assert False
+
+class TestMoveUp:
+
+    def test_move_up(self):
+        assert False
+
+    def test_move_up_on_fixed_board(self):
+        assert False
+
+    def test_move_up_and_game_lost(self):
+        assert False
+
+    def test_move_up_and_game_won(self):
+        assert False
+
+    def test_move_up_prev_board_equals_non_fixed_board(self):
+        assert False
+
+    def test_move_up_prev_board_equals_fixed_board(self):
+        assert False
+
+class TestMoveDown:
+
+    def test_move_down(self):
+        assert False
+
+    def test_move_down_on_fixed_board(self):
+        assert False
+
+    def test_move_down_and_game_lost(self):
+        assert False
+
+    def test_move_down_and_game_won(self):
+        assert False
+
+    def test_move_down_prev_board_equals_non_fixed_board(self):
+        assert False
+
+    def test_move_down_prev_board_equals_fixed_board(self):
+        assert False
+
+class TestUndo:
+    
+    def test_undo_move(self):
+        assert False
